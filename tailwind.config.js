10cb2d3186dca43fd6c52de344c99a2b4adc5b9e
Hanine122 <Hammamihanine6@gmail.com>
2026-05-21 14:45:43 +0100

    feat: layout changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './Views/**/*.cshtml',
+    './wwwroot/**/*.js',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
